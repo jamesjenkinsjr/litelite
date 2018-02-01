1f55dbf247cdad63b6bb77e2de5b0ea7735ded52
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-01 18:52:58 -0500

    Grid size selector form

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //variables
 const resetButton = document.querySelector('#reset');
 const boxes = document.querySelectorAll('.box');
+const gridSizeForm = document.querySelector('#grid-size');
 
 //eventListeners
 boxes.forEach(function(box, index){
     addEventListener('click', toggleRed);    
 })
-resetButton.addEventListener('click', resetBoard)
+resetButton.addEventListener('click', resetBoard);
+gridSizeForm.addEventListener('submit', changeGrid);
 
 //functions
 
@@ -19,4 +21,10 @@ function resetBoard(e) {
     boxes.forEach((box, index) => {
         box.classList.remove('red');
     });
+}
+
+function changeGrid(e) {
+    e.preventDefault();
+    const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
+    console.log(e);
 }
